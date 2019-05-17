1bb00d6a90a3261a0fbd52f542294b7f88ba86e1
Chris Valenzuela <chriss.valenzuela@yahoo.com>
2019-05-16 19:13:12 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,25 @@ var keys = require("../keys"); // keys.keys => password
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: keys.keys,
-    database: "burgers_db"
-})
+// JawsdB heroku so if theres a jawsdb connect to it. if not go to the local one
+var connection;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{ 
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: keys.keys,
+        database: "burgers_db"
+    })
+
+}
+
+
+
+
 
 connection.connect(function(err){
     if (err) throw err;
